a5f248b731a2c65af5ff96a3fa88be4e4678b55c
Grashal Vishwa <89205098+vgrashal16@users.noreply.github.com>
2024-01-22 15:48:14 +0530

    Details page created, toggleable button to show additional data created

--- vite-project/src/pages/detail.tsx
+++ vite-project/src/pages/detail.tsx
@@ -1,15 +1,38 @@
 import { Background } from './homepage'
 import { useLocation } from 'react-router-dom'
 import { Button, Box } from '@mui/material';
+import { useState } from 'react';
 
 
 function Detail() {
   const location = useLocation();
   const apiData = location.state.apiData
-  const handleWeather = () => {
-    console.log('clicked');
+  const API_KEY = '97d09493a27e75636d61161cdff20e78';
+
+  const fetchData = async() => {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${apiData[0].latlng[0]}&lon=${apiData[0].latlng[1]}&appid=${API_KEY}`);
+    const weatherData = await res.json();
+    return weatherData;
   }
+
+  const [isWeatherVisible, setWeatherVisible] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleWeather = async () => {
+    if (isWeatherVisible) {
+      setWeatherVisible(false);
+      setWeatherData(null);
+    } 
+    
+    else {
+      const data = await fetchData();
+      setWeatherData(data);
+      setWeatherVisible(true);
+    }
+  };
+
   const flag_img = apiData[0].flags.png;
+
   return (
     <Background>
     <Box sx={{display: 'flex',flexDirection: 'column',height: '100vh',width: '100vw',}}>
@@ -17,17 +40,23 @@ function Detail() {
         <img src={flag_img}/>
         <p>{apiData[0].name.common.toUpperCase()}</p>
       </Box>
-      <Box sx={{display: 'flex',height: '70%',flexDirection: 'row',width: '100%',justifyContent: 'center',alignItems: 'center',}}>
+      <Box sx={{display: 'flex',height: '60%',flexDirection: 'row',width: '100%',justifyContent: 'center',alignItems: 'center',}}>
         <Box sx={{display: 'flex',height: '100%',flexDirection: 'column',alignItems: 'center',width: '70%', fontSize: '1.3em'}}>
           <p>Capital: {apiData[0].capital[0]}</p>   
           <p>Population: {apiData[0].population}</p>
-          <p>Latitude: {apiData[0].latlng[0]}</p>
-          <p>Longitude: {apiData[0].latlng[1]}</p>
+          <p>Latitude: {apiData[0].latlng[0]} °</p>
+          <p>Longitude: {apiData[0].latlng[1]} °</p>
         </Box>
-        <Box sx={{display: 'flex',height: '100%',width: '30%',justifyContent: 'center'}}>
-          <Button variant="contained" color="secondary" sx={{ color: 'aliceblue', height: '40px'}} onClick={handleWeather}>
-            Show Weather
+        <Box sx={{display: 'flex',height: '100%',width: '40%', flexDirection: 'column', fontSize: '1.3em'}}>
+          <Button variant="contained" color="secondary" sx={{ color: 'aliceblue', height: '40px', width: '160px'}} onClick={handleWeather}>  
+            {isWeatherVisible ? 'Hide Weather' : 'Show Weather'}
           </Button>
+          {isWeatherVisible && (
+            <>
+              <p>Wind Speed: {weatherData ? weatherData.wind.speed : 'Loading...'} mph</p>
+              <p>Temperature: {weatherData ? (weatherData.main.temp-273.15).toFixed(2) : 'Loading...'} °C</p>
+            </>
+            )}
         </Box>
       </Box>
     </Box>
